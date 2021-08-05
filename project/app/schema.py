4eef36bb00057c192098d77779de9a56bfa257bb
greg goldman <gregory.goldman97@gmail.com>
2021-08-05 13:50:12 -0400

    Update and Delete

--- project/app/schema.py
+++ project/app/schema.py
@@ -32,8 +32,42 @@ class CreateLink(graphene.Mutation):
             id=link.id,
             url=link.url,
             description=link.description,
+        
         )
+class UpdateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
 
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return UpdateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        
+        )
+class DeleteLink(graphene.Mutation):
+    id = graphene.Int()
+
+    class Arguments:
+        id = graphene.Int()
+
+    def mutate(self, info, id):
+        link = Link(id=id)
+        link.delete()
+
+        return DeleteLink(
+            id=link.id      
+        )
 
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    update_link = UpdateLink.Field()
+    delete_link = DeleteLink.Field()
